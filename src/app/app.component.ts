aae00270a9d7cdd8785fd229fcd3308c211d6404
jdcera4 <jdcera4@gmail.com>
2022-05-02 11:36:58 -0500

    create form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,65 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'form-register';
+  pais = '';
+  primerNombre = '';
+  segundoNombre = '';
+  genero = '';
+  nacimiento = '';
+  tipoDocumento = '';
+  numeroDocumento = 0;
+  fotoDocumentoFrente = '';
+  fotoDocumentoReverso = '';
+
+  siguiente() {
+    if (this.numeroDocumento <= 12345) {
+      alert('falta por completar');
+    }
+    if (this.pais.length === 0) {
+      alert('falta por completar');
+    }
+    if (this.primerNombre.length === 0) {
+      alert('falta por completar');
+    }
+    if (this.segundoNombre.length === 0) {
+      alert('falta por completar');
+    }
+    if (this.genero.length === 0) {
+      alert('falta por completar');
+    }
+    if (this.nacimiento.length === 0) {
+      alert('falta por completar');
+    }
+    if (this.tipoDocumento.length === 0) {
+      alert('falta por completar');
+    }
+    if (this.fotoDocumentoFrente.length === 0) {
+      alert('falta por completar');
+    }
+    if (this.fotoDocumentoReverso.length === 0) {
+      alert('falta por completar');
+    }
+    console.log(
+      'pais: ',
+      this.pais,
+      'genero: ',
+      this.genero,
+      'primer nombre: ',
+      this.primerNombre,
+      'segundo nobre: ',
+      this.segundoNombre,
+      'fecha de nacimiento: ',
+      this.nacimiento,
+      'Tipo de documento: ',
+      this.tipoDocumento,
+      'foto documento frente: ',
+      this.fotoDocumentoFrente,
+      'foto documento reverso: ',
+      this.fotoDocumentoReverso
+    );
+  }
 }
